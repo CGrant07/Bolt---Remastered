5b9b1eb42d4f6b9bf9f6630c8b9df6ea7d786830
Grant <carsongrant778@gmail.com>
2024-01-31 08:09:48 -0500

    Start - Speaking & Text

---
+++ Bolt.js
@@ -0,0 +1,15 @@
+//Team Members: Carson Grant, Cade Smith
+//Name: Carson Grant
+//Date: 1.22.2024
+//Program: Sphero Bolt Maze
+//Bolt ID: B2F8
+
+//Start Commit
+
+async function startProgram() {
+
+	setMainLed({ r: 255, g: 255, b: 255 });
+
+	await speak("Start", true); 
+
+	
