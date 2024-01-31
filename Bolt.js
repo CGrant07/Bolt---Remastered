a980816ab02c7c69731477e0e0ef4600ef5dd1c1
Grant <carsongrant778@gmail.com>
2024-01-31 08:20:36 -0500

    Sound 3 & Purple LED Commit

--- Bolt.js
+++ Bolt.js
@@ -75,3 +75,22 @@ async function startProgram() {
 	await scrollMatrixText('Green!', { r: 0, g: 255, b: 0 }, 30, true) 
 	
 	setMainLed({ r: 0, g:255 , b:0  }); 
+
+	//Sound 3 & Purple LED Commit
+
+	await roll((getHeading() + -91), 60, 1.1) 
+	
+	await delay (1);
+	
+	await roll((getHeading() + -40), 60, 1.2) 
+	
+	setMainLed({ r: 255, g:0 , b:255 });
+	
+	await delay (1);
+	
+	await Sound.Animal.Monkey.play(true); 
+
+	await scrollMatrixText('Purple!', { r: 255, g: 0, b: 255 }, 30, true) 
+	
+	await delay (1);
+	
